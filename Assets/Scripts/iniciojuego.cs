dab9045c4f4e79e8003a6b083d415980cd989904
Nacho2126 <ignaciogp26@gmail.com>
2015-11-05 17:43:28 -0300

    Creacion Menu - Boton jugar y boton opciones
    Creacion Menu - Boton jugar y boton opciones.. falta credito y salir

---
+++ Assets/Scripts/iniciojuego.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class iniciojuego : MonoBehaviour {
+
+	public Animator BotonJugar;
+	public Animator BotonOpnciones;
+	public Animator BotonCreditos;
+	public Animator BotonSalir;
+	public Animator panelOpciones;
+
+	public void CambiarEscena (string Main)
+	{
+		Application.LoadLevel (Main); 
+	}
+
+	public void EsconderMenu (bool esconder){
+	
+		BotonJugar.SetBool ("esconder", esconder);
+		BotonOpnciones.SetBool ("esconder", esconder);
+		BotonCreditos.SetBool ("esconder", esconder);
+		BotonSalir.SetBool ("esconder", esconder);
+	}
+
+	public void EsconderOpciones (bool esconder){
+	
+		panelOpciones.enabled = true;
+		panelOpciones.SetBool ("esconder", esconder);
+	}
+
+}
+
